315f6b2c4263be9baa862d7ab65699c19e860c5f
Roshan Mahanama <roshan@Roshans-MacBook-Pro.local>
2015-04-16 12:04:30 +1000

    Initial Commit

---
+++ Blocstagram/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Blocstagram
+//
+//  Created by Roshan Mahanama on 16/04/2015.
+//  Copyright (c) 2015 RMTREKS. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Blocstagram/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Blocstagram
+//
+//  Created by Roshan Mahanama on 16/04/2015.
+//  Copyright (c) 2015 RMTREKS. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
