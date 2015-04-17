36627542dfcfc54e898d0f499f17fbc94b341a8d
Roshan Mahanama <roshan@pc97.wifi2209.mar.matrix>
2015-04-17 12:16:49 +1000

    code entered as per checkpoint

--- Blocstagram/BLCImagesTableViewController.m
+++ Blocstagram/BLCImagesTableViewController.m
@@ -11,6 +11,7 @@
 #import "BLCMedia.h"
 #import "BLCUser.h"
 #import "BLCComment.h"
+#import "BLCMediaTableViewCell.h"
 
 @interface BLCImagesTableViewController ()
 
@@ -32,7 +33,7 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     
-    [self.tableView registerClass:[UITableViewCell class] forCellReuseIdentifier:@"imageCell"];
+    [self.tableView registerClass:[BLCMediaTableViewCell class] forCellReuseIdentifier:@"mediaCell"];
 }
 
 - (void)didReceiveMemoryWarning {
@@ -52,27 +53,8 @@
 
  - (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
  {
- 
-     UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"imageCell" forIndexPath:indexPath];
-     
-     // Configure the cell...
-     static NSInteger imageViewTag = 1234;
-     UIImageView *imageView = (UIImageView*)[cell.contentView viewWithTag:imageViewTag];
-     
-     if (!imageView) {
-         // This is a new cell, it doesn't have an image view yet
-         imageView = [[UIImageView alloc] init];
-         imageView.contentMode = UIViewContentModeScaleToFill;
-         
-         imageView.frame = cell.contentView.bounds;
-         imageView.autoresizingMask = UIViewAutoresizingFlexibleHeight | UIViewAutoresizingFlexibleWidth;
-         
-         imageView.tag = imageViewTag;
-         [cell.contentView addSubview:imageView];
-     }
-     
-     BLCMedia *item = [self items][indexPath.row];
-     imageView.image = item.image;
+     BLCMediaTableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"mediaCell" forIndexPath:indexPath];
+     cell.mediaItem = [BLCDataSource sharedInstance].mediaItems[indexPath.row];
      
      return cell;
  }
@@ -81,12 +63,7 @@
 - (CGFloat) tableView:(UITableView *)tableView heightForRowAtIndexPath:(NSIndexPath *)indexPath {
     
     BLCMedia *item = [self items][indexPath.row];
-    UIImage *image = item.image;
-    
-//    NSLog(@"%lu", [self items].count); // checking that the function works
-    
-    return (CGRectGetWidth(self.view.frame) / image.size.width) * image.size.height;
-
+    return [BLCMediaTableViewCell heightForMediaItem:item width:CGRectGetWidth(self.view.frame)];
 }
 
 
---
+++ Blocstagram/BLCMediaTableViewCell.h
@@ -0,0 +1,20 @@
+//
+//  BLCMediaTableViewCell.h
+//  Blocstagram
+//
+//  Created by Roshan Mahanama on 16/04/2015.
+//  Copyright (c) 2015 RMTREKS. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class BLCMedia;
+
+@interface BLCMediaTableViewCell : UITableViewCell
+
+@property (nonatomic, strong) BLCMedia *mediaItem;
+
++ (CGFloat) heightForMediaItem:(BLCMedia *)mediaItem width:(CGFloat)width;
+
+
+@end
