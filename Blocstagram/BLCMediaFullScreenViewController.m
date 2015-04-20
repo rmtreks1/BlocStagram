6849689529b05572735cde7798e7cb133bd7208f
Roshan Mahanama <rmtreks@gmail.com>
2015-04-21 08:32:09 +1000

    more code entered as per checkpoint

--- Blocstagram/BLCMediaFullScreenViewController.m
+++ Blocstagram/BLCMediaFullScreenViewController.m
@@ -37,21 +37,82 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view.
+    
+    self.scrollView = [UIScrollView new]; // is this an alternative to Alloc/Init
+    self.scrollView.delegate = self;
+    self.scrollView.backgroundColor = [UIColor whiteColor];
+    
+    [self.view addSubview:self.scrollView];
+    
+    self.imageView = [UIImageView new];
+    self.imageView.image = self.media.image;
+    
+    [self.scrollView addSubview:self.imageView];
+    self.scrollView.contentSize = self.media.image.size;
+    
 }
+
+
+- (void) viewWillLayoutSubviews {
+    [super viewWillLayoutSubviews];
+    self.scrollView.frame = self.view.bounds;
+    
+    CGSize scrollViewFrameSize = self.scrollView.frame.size;
+    CGSize scrollViewContentSize = self.scrollView.contentSize;
+    
+    CGFloat scaleWidth = scrollViewFrameSize.width / scrollViewContentSize.width;
+    CGFloat scaleHeight = scrollViewFrameSize.height / scrollViewContentSize.height;
+    CGFloat minScale = MIN(scaleWidth, scaleHeight);
+    
+    self.scrollView.minimumZoomScale = minScale;
+    self.scrollView.maximumZoomScale = 1;
+}
+
+
+- (void) viewWillAppear:(BOOL)animated {
+    [super viewWillAppear:animated];
+    
+    [self centerScrollView];
+}
+
+
+- (void)centerScrollView {
+    [self.imageView sizeToFit];
+    
+    CGSize boundsSize = self.scrollView.bounds.size;
+    CGRect contentsFrame = self.imageView.frame;
+    
+    if (contentsFrame.size.width < boundsSize.width) {
+        contentsFrame.origin.x = (boundsSize.width - CGRectGetWidth(contentsFrame)) / 2;
+    } else {
+        contentsFrame.origin.x = 0;
+    }
+    
+    if (contentsFrame.size.height < boundsSize.height) {
+        contentsFrame.origin.y = (boundsSize.height - CGRectGetHeight(contentsFrame)) / 2;
+    } else {
+        contentsFrame.origin.y = 0;
+    }
+    
+    self.imageView.frame = contentsFrame;
+}
+
+
+
+#pragma mark - UIScrollViewDelegate
+
+- (UIView*)viewForZoomingInScrollView:(UIScrollView *)scrollView {
+    return self.imageView;
+}
+
+- (void)scrollViewDidZoom:(UIScrollView *)scrollView {
+    [self centerScrollView];
+}
+
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
-
-/*
-#pragma mark - Navigation
-
-// In a storyboard-based application, you will often want to do a little preparation before navigation
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
-}
-*/
 
 @end
