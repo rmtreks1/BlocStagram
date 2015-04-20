67d8931fc70755393c5f1af7d35fc4a055f15264
Roshan Mahanama <rmtreks@gmail.com>
2015-04-20 17:00:34 +1000

    code entered as per checkpoint BUT issue - images are not appearing

--- Blocstagram/AppDelegate.m
+++ Blocstagram/AppDelegate.m
@@ -24,22 +24,33 @@
     [BLCDataSource sharedInstance];
     
     UINavigationController *navVC = [[UINavigationController alloc] init];
-    BLCLoginViewController *loginVC = [[BLCLoginViewController alloc] init];
-    [navVC setViewControllers:@[loginVC] animated:YES];
-//    loginVC.title = @"Login";
     
-    
-//    // setting a back button on the left
-//    UIBarButtonItem *backToLogin = [[UIBarButtonItem alloc] initWithTitle:@"BACK!!!" style:UIBarButtonItemStylePlain target:self action:nil];
-//    
-    
-    
-    
-    
-    [[NSNotificationCenter defaultCenter] addObserverForName:BLCLoginViewControllerDidGetAccessTokenNotification object:nil queue:nil usingBlock:^(NSNotification *note) {
-        BLCImagesTableViewController *imagesVC = [[BLCImagesTableViewController alloc] init];
+    if(![BLCDataSource sharedInstance].accessToken){
+        BLCLoginViewController *loginVC = [[BLCLoginViewController alloc] init];
+        [navVC setViewControllers:@[loginVC] animated:YES];
+        //    loginVC.title = @"Login";
+        
+        
+        //    // setting a back button on the left
+        //    UIBarButtonItem *backToLogin = [[UIBarButtonItem alloc] initWithTitle:@"BACK!!!" style:UIBarButtonItemStylePlain target:self action:nil];
+        //
+        
+        
+        
+        
+        [[NSNotificationCenter defaultCenter] addObserverForName:BLCLoginViewControllerDidGetAccessTokenNotification object:nil queue:nil usingBlock:^(NSNotification *note) {
+            BLCImagesTableViewController *imagesVC = [[BLCImagesTableViewController alloc] init];
+            [navVC setViewControllers:@[imagesVC] animated:YES];
+        }];
+    } else {
+        BLCImagesTableViewController *imagesVC = [[BLCImagesTableViewController alloc]init];
         [navVC setViewControllers:@[imagesVC] animated:YES];
-    }];
+    }
+    
+    
+    
+    
+    
     
     self.window.rootViewController = navVC;
     
