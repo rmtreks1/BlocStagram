b93b6cc699a0ee30dfc082fcda3b0e31c5749f7a
Roshan Mahanama <rmtreks@gmail.com>
2015-04-26 13:34:14 +1000

    code entered as per checkpoint up to Introducing Categories

---
+++ Blocstagram/BLCCameraToolbar.h
@@ -0,0 +1,27 @@
+//
+//  BLCCameraToolbar.h
+//  Blocstagram
+//
+//  Created by Roshan Mahanama on 26/04/2015.
+//  Copyright (c) 2015 RMTREKS. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class BLCCameraToolbar;
+
+@protocol BLCCameraToolbarDelegate <NSObject>
+
+- (void) leftButtonPressedOnToolbar:(BLCCameraToolbar *)toolbar;
+- (void) rightButtonPressedOnToolbar:(BLCCameraToolbar *)toolbar;
+- (void) cameraButtonPressedOnToolbar:(BLCCameraToolbar *)toolbar;
+
+@end
+
+@interface BLCCameraToolbar : UIView
+
+- (instancetype) initWithImageNames:(NSArray *)imageNames;
+
+@property (nonatomic, weak) NSObject <BLCCameraToolbarDelegate> *delegate;
+
+@end
