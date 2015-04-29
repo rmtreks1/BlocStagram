72a581fc00e09393f352122ee60185b98873ec1e
Roshan Mahanama <rmtreks@gmail.com>
2015-04-29 18:49:41 +1000

    mediatableviewcelltests - structure set up - not functional

---
+++ BlocstagramTests/BLCMediaTableViewCellTests.m
@@ -0,0 +1,38 @@
+//
+//  BLCMediaTableViewCellTests.m
+//  Blocstagram
+//
+//  Created by Roshan Mahanama on 29/04/2015.
+//  Copyright (c) 2015 RMTREKS. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <XCTest/XCTest.h>
+#import "BLCMediaTableViewCell.h"
+
+
+@interface BLCMediaTableViewCellTests : XCTestCase
+
+@end
+
+@implementation BLCMediaTableViewCellTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
